7a7dfe12887e7fa1b6ae3c2881bfa17804d7ba2e
LasseTimmermanns <lasse.mainkonto@gmail.com>
2023-03-25 12:28:22 +0100

    added adminpanel design

--- admin.php
+++ admin.php
@@ -5,16 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style/home.css">
+    <link rel="stylesheet" href="style/admin.css">
 </head>
 <body>
     <?php
         session_start();
-        if($_SESSION["loggedin"] != "true"){
-            echo("not logged in");
-        }else{
-            echo("logged in");
+        if(!(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] == "true")){
+            header("Location: login_form.php");          
         }
-
     ?>
+
+    <div class="bigwrapper">
+        <div class="wrapper">
+            <div class="big create"><img src="img/add.svg"></div>
+            <div class="big update"><img src="img/update.svg"></div>
+            <div class="big delete"><img src="img/delete.svg"></div>
+        </div>
+    </div>
 </body>
 </html>
