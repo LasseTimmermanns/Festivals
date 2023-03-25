a5e706b022d23f07ada2b8995805709f4da214ea
IchBinIch-chich <ruben.andresen@gmx.de>
2023-03-25 13:41:11 +0100

    implemented the sql db into index.php

--- index.php
+++ index.php
@@ -7,10 +7,32 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>
     <?php
-        echo("Hey")
+        $servername = "localhost";
+        $username = "festival";
+        $password = "YUcrXDm87tMzmSh";
+        $dbname = "festivals";
+        
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+          die("Connection failed: " . mysqli_connect_error());
+        }
+        
+        $sql = "SELECT festivalname, ort, preis, datum, erstellungsdatum FROM festivals";
+        $result = mysqli_query($conn, $sql);
+        
+        if (mysqli_num_rows($result) > 0) {
+          // output data of each row
+          while($row = mysqli_fetch_assoc($result)) {
+            // echo "Name: " . $row["festivalname"]. " - Ort: " . $row["ort"]. " - Preis: " . $row["preis"]. " - Datum: " . $row["datum"]. " - Erstellungsdatum: " . $row["erstellungsdatum"]. "<br>";
+          }
+        } else {
+          echo "0 results";
+        }
+        
+        mysqli_close($conn);
     ?>
-    </h1>
 </body>
 </html>
